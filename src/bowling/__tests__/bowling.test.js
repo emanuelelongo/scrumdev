b36cdfdd8e2d2cdf5b67e1d88a2b5bfbe7052390
Emanuele Longo <emanuele.longo@ynap.com>
2017-05-24 18:30:31 +0200

    WIP

--- src/bowling/__tests__/bowling.test.js
+++ src/bowling/__tests__/bowling.test.js
@@ -1,5 +1,42 @@
-import { score } from '../bowling'
+import score, { scoreFrame } from '../bowling'
 
 test('should return a number score given a string', () => {
     expect(typeof score('')).toEqual('number')
+})
+
+test('should score a strike frame', () => {
+    expect(scoreFrame('X', [10, 5])).toEqual({
+        pins: 10,
+        score: 25,
+        bonus: 2
+    })
+})
+
+test('should score a spare frame', () => {
+    expect(scoreFrame('3/', [10, 5])).toEqual({
+        pins: 3,
+        score: 25,
+        bonus: 1
+    })
+    expect(scoreFrame('9/', [10, 5])).toEqual({
+        pins: 9,
+        score: 10,
+        bonus: 1
+    })
+})
+
+test('should score a miss frame', () => {
+    expect(scoreFrame('5-', [10, 5])).toEqual({
+        pins: 5,
+        score: 5,
+        bonus: 0
+    })
+})
+
+test('should score a double miss frame', () => {
+    expect(scoreFrame('--', [10, 5])).toEqual({
+        pins: 0,
+        score: 0,
+        bonus: 0
+    })
 })
--- src/bowling/bowling.js
+++ src/bowling/bowling.js
@@ -1,3 +1,37 @@
-export function score(game) {
+export default function score(game) {
     return 0
+}
+
+export function scoreFrame(frame, nexts) {
+    if(frame === 'X') {
+        return {
+            pins: 10,
+            score: 10 + (nexts[0] || 0) + (nexts[1] || 0),
+            bonus: 2
+        }
+    }
+
+    if(frame.indexOf('/') === 1) {
+        return {
+            pins: parseInt(frame[0]),
+            score: 10 + (nexts[0] || 0),
+            bonus: 1
+        }
+    }
+
+    if(frame.indexOf('-') === 1) {
+        return {
+            pins: parseInt(frame[0]),
+            score: parseInt(frame[0]),
+            bonus: 0
+        }
+    }
+
+    if(frame.indexOf('-') === 0) {
+        return {
+            pins: 0,
+            score: 0,
+            bonus: 0
+        }
+    }
 }
